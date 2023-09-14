8517c8f7679a617450251d6ae7109fa808c4f344
Zeionn <87084175+YukoKatsuki@users.noreply.github.com>
2023-09-14 19:44:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 video = "";
+status = "";
+objects = [];
 
 function preload() {
     video = createVideo('video.mp4');
@@ -6,12 +8,35 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(480, 380);
+    canvas = createCanvas(310, 280);
     canvas.center();
 }
 
 function draw() {
-    image(video, 0, 0, 480, 380);
+    image(video, 0, 0, 310, 280);
+        if(status !="")
+        {
+            objectDetector.detect(video, gotResult);
+            for (i = 0; i < objects.length; i++) {
+                document.getElementById("status").innerHTML = "Status: Objetos Detectados";
+                document.getElementById("numberOfObjects").innerHTML = "Quantidade de Objetos Detectados: "+ objects.length;
+
+                fill("#FF0000");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+        }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() 
